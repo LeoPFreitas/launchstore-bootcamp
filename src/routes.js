c369cce426fbc72b6b8233c793b8328214af89fe
LeoPFreitas <leonardo.cadu2@hotmail.com>
2020-01-24 19:15:59 -0300

    Add search page style

--- src/routes.js
+++ src/routes.js
@@ -4,9 +4,15 @@ const multer = require('./app/middlewares/multer')
 
 const ProductController = require('./app/controllers/ProductController')
 const HomeController = require('./app/controllers/HomeController')
+const SearchController = require('./app/controllers/SearchController.js')
 
+//  HOME
 routes.get('/', HomeController.index)
 
+//  SEARCH
+routes.get('/products/search', SearchController.index)
+
+//  PRODUCTS 
 routes.get('/products/create', ProductController.create)
 routes.get('/products/:id', ProductController.show)
 routes.get('/products/:id/edit', ProductController.edit)
@@ -15,11 +21,11 @@ routes.post('/products', multer.array("photos", 6), ProductController.post)
 routes.put('/products', multer.array("photos", 6), ProductController.put)
 routes.delete('/products', ProductController.delete)
 
+
 // ALIAS
 routes.get('/ads/create', function(req, res){
     return res.redirect("/products/create")
 })
-
 
 module.exports = routes
 
