8d0f6c4627eebdd07452a012534a8eb54e78d744
LeoPFreitas <leonardo.cadu2@hotmail.com>
2020-01-24 17:20:48 -0300

    Add product image lightbox

--- public/scripts.js
+++ public/scripts.js
@@ -78,7 +78,7 @@ const PhotosUpload = {
 
     // para caso de somar mais de 6 fotos
     const totalPhotos = fileList.length + photosDivs.length
-    if(totalPhotos > uploadLimit){
+    if (totalPhotos > uploadLimit) {
       alert("Você atingio o limite máximo de fotos")
       event.preventDefault()
       return true
@@ -125,4 +125,38 @@ const PhotosUpload = {
     }
     photoDiv.remove()
   }
+}
+
+const ImageGallery = {
+  highlight: document.querySelector('.gallery .highlight > img'),
+  previews: document.querySelectorAll('.gallery-preview img'),
+  setImage(e) {
+    const { target } = e
+
+    ImageGallery.previews.forEach(preview => preview.classList.remove('active'))
+    target.classList.add('active')
+
+    ImageGallery.highlight.src = target.src
+    Lightbox.image.src = target.src
+  }
+}
+
+const Lightbox = {
+  target: document.querySelector('.lightbox-target'),
+  image: document.querySelector('.lightbox-target img'),
+  closeButton: document.querySelector('.lightbox-target a.lightbox-close'),
+  open() {
+    Lightbox.target.style.opacity = 1
+    Lightbox.target.style.top = 0
+    Lightbox.target.style.bottom = 0
+    Lightbox.closeButton.style.top = "5%"
+    Lightbox.closeButton.style.right = '5%'
+  },
+  close() {
+    Lightbox.target.style.opacity = 0
+    Lightbox.target.style.top = '-100%'
+    Lightbox.target.style.bottom = 'initial'
+    Lightbox.closeButton.style.top = '-10%'
+    Lightbox.closeButton.style.right = '-10%'
+   }
 }
