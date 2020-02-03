9526cee71f0ea5448b86b9773edac30a22695f82
LeoPFreitas <leonardo.cadu2@hotmail.com>
2020-02-03 18:59:21 -0300

    Add validation functions to register fields

--- public/scripts.js
+++ public/scripts.js
@@ -15,32 +15,30 @@ const Mask = {
   cpfCnpj(value) {
     value = value.replace(/\D/g, "");
 
-    if (value.length > 14)
-      value = value.slice(0, -1)
+    if (value.length > 14) value = value.slice(0, -1);
 
     // check if is cfp
     if (value.length > 11) {
       //cnpj 11.222.333/0001-11 --> 14 digitos
-      value = value.replace(/(\d{2})(\d{3})(\d{3})(\d{4})(\d{2})/, "$1.$2.$3/$4-$5")
+      value = value.replace(
+        /(\d{2})(\d{3})(\d{3})(\d{4})(\d{2})/,
+        "$1.$2.$3/$4-$5"
+      );
     } else {
       //cpf 470.654.449-33
-      value = value.replace(/(\d{3})(\d{3})(\d{3})(\d{2})/, "$1.$2.$3-$4")
+      value = value.replace(/(\d{3})(\d{3})(\d{3})(\d{2})/, "$1.$2.$3-$4");
     }
 
-    return value
+    return value;
   },
-  cep(value){
-
+  cep(value) {
     value = value.replace(/\D/g, "");
 
-    if (value.length > 8)
-      value = value.slice(0, -1)
+    if (value.length > 8) value = value.slice(0, -1);
 
-    value = value.replace(/(\d{5})(\d)/, "$1-$2")
+    value = value.replace(/(\d{5})(\d)/, "$1-$2");
 
-
-    return value
-
+    return value;
   }
 };
 
@@ -188,5 +186,76 @@ const Lightbox = {
     Lightbox.target.style.bottom = "initial";
     Lightbox.closeButton.style.top = "-10%";
     Lightbox.closeButton.style.right = "-10%";
+  }
+};
+
+const Validate = {
+  apply(input, func) {
+    //  limpar os campos de error
+    Validate.clearErros(input)
+
+    let results = Validate[func](input.value);
+    input.value = results.value
+
+    if (results.error){
+      Validate.displayError(input, results.error)
+    }
+  },
+  displayError(input, error){
+    const div = document.createElement('div')
+    div.classList.add('error')
+    div.innerHTML = error
+    input.parentNode.appendChild(div)
+    input.focus()
+  },
+  clearErros(input){
+    const errorDiv = input.parentNode.querySelector('.error')
+    if (errorDiv)
+      errorDiv.remove()
+  },
+  isEmail(value){
+    let error = null
+    // abcd - 
+    const mailFormat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
+
+    if (!value.match(mailFormat))
+      error = "Email inválido"
+
+    return {
+      error,
+      value
+    }
+  },
+  isCpfCnpj(value){
+    let error = null
+
+    const cleanValues = value.replace(/\D/g, "")
+
+    if (cleanValues.length < 12 && cleanValues.length !== 11) {
+      error = "CPF inválido"
+    }
+
+    else if (cleanValues.length > 12 && cleanValues.length !== 14){
+      error = "CNPJ incorreto"
+    }
+
+    return {
+      error,
+      value
+    }
+  },
+  isCep(value){
+    let error = null
+
+    const cleanValues = value.replace(/\D/g, "")
+
+    if (cleanValues.length !== 8) {
+      error = "CEP inválido"
+    }
+    
+    return {
+      error,
+      value
+    }
   }
 };
