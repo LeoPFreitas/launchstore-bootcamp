2383b76154b216a70679a0189be364e119b49959
LeoPFreitas <leonardo.cadu2@hotmail.com>
2020-02-03 20:55:01 -0300

    Change styles and add message s part

--- src/app/validators/user.js
+++ src/app/validators/user.js
@@ -1,32 +1,43 @@
-const User = require('../models/User')
-async function post(req, res, next){
+const User = require("../models/User");
+async function post(req, res, next) {
   //check if has all fields
   const keys = Object.keys(req.body);
 
-  for (key of keys) { 
+  for (key of keys) {
     if (req.body[key] == "") {
-      return res.send("Please, fill all fields!");
+      return res.render("user/register", {
+        user: req.body,
+        error: "Por favor, preencha todos os campos."
+      });
     }
   }
 
-  // check if user exists [email, cpf_cnpj] 
-  let { email, cpf_cnpj, password, passwordRepeat } = req.body
+  // check if user exists [email, cpf_cnpj]
+  let { email, cpf_cnpj, password, passwordRepeat } = req.body;
 
-  cpf_cnpj = cpf_cnpj.replace(/\D/g, "")
+  cpf_cnpj = cpf_cnpj.replace(/\D/g, "");
 
-  const user = await User.findOne({ 
+  const user = await User.findOne({
     where: { email },
-    or: { cpf_cnpj } 
-  })
+    or: { cpf_cnpj }
+  });
 
-  if (user) return res.send("User exist")
-  
+  if (user)
+    return res.render("user/register", {
+      user: req.body,
+      error: "usuário já cadastrado!"
+    });
+
   // check if password match
-  if (password != passwordRepeat) return res.send("Password Mismatch")
+  if (password != passwordRepeat)
+    return res.render("user/register", {
+      user: req.body,
+      error: "A senha e a repetição da senha estão incorretas."
+    });
 
-  next()
+  next();
 }
 
 module.exports = {
   post
-}
+};
