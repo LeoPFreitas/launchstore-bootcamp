abf231a391801871b6d63921b3b89c591d448246
LeoPFreitas <leonardo.cadu2@hotmail.com>
2020-02-03 21:26:04 -0300

    Add password encryption

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -3,8 +3,13 @@ module.exports = {
   registerForm(req, res) {
     return res.render("user/register");
   },
+  show(req, res){
+    return res.send('ok, cadastrado')
+  },
   async post(req, res) {
 
-    return res.send("Passed!")
+    const userId = await User.create(req.body)
+
+    return res.redirect('/users')
   }
 };
