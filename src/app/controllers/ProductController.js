19208983e7534998c195c1af7434542606bad39e
LeoPFreitas <leonardo.cadu2@hotmail.com>
2020-02-09 19:23:34 -0300

    Add services and refactor controllers

--- src/app/controllers/ProductController.js
+++ src/app/controllers/ProductController.js
@@ -1,10 +1,9 @@
-const { unlinkSync } = require('fs');
+const { unlinkSync } = require("fs");
 
 const Category = require("../models/Category");
 const Product = require("../models/Product");
 const File = require("../models/File");
-
-const { formatPrice, date } = require("../../lib/utils");
+const LoadProductService = require("../services/LoadProductService");
 
 module.exports = {
   async create(req, res) {
@@ -63,57 +62,29 @@ module.exports = {
   },
   async show(req, res) {
     try {
-      const product = await Product.find(req.params.id);
+      const product = await LoadProductService.load("product", {
+        where: {
+          id: req.params.id
+        }
+      });
 
-      if (!product) return res.send("Product Not Found!");
-
-      const { day, hour, minutes, month } = date(product.updated_at);
-
-      product.published = {
-        day: `${day}/${month}`,
-        hour: `${hour}h${minutes}`
-      };
-
-      product.oldPrice = formatPrice(product.old_price);
-      product.price = formatPrice(product.price);
-
-      let files = await Product.files(product.id);
-      files = files.map(file => ({
-        ...file,
-        src: `${req.protocol}://${req.headers.host}${file.path.replace(
-          "public",
-          ""
-        )}`
-      }));
-
-      return res.render("products/show", { product, files });
+      return res.render("products/show", { product });
     } catch (error) {
       console.error(error);
     }
   },
   async edit(req, res) {
     try {
-      const product = await Product.find(req.params.id);
-
-      if (!product) return res.send("Product not found!");
-
-      product.old_price = formatPrice(product.old_price);
-      product.price = formatPrice(product.price);
+      const product = await LoadProductService.load("product", {
+        where: {
+          id: req.params.id
+        }
+      });
 
       // get categories
       const categories = await Category.findAll();
 
-      // get images
-      let files = await Product.files(product.id);
-      files = files.map(file => ({
-        ...file,
-        src: `${req.protocol}://${req.headers.host}${file.path.replace(
-          "public",
-          ""
-        )}`
-      }));
-
-      return res.render("products/edit", { product, categories, files });
+      return res.render("products/edit", { product, categories });
     } catch (error) {
       console.error(error);
     }
@@ -170,13 +141,10 @@ module.exports = {
     }
   },
   async delete(req, res) {
-    // pega todos aquivos de produto
     const files = await Product.files(req.body.id);
 
-    // deleta do bb
     await Product.delete(req.body.id);
 
-    // remover as imagens da pasta public
     files.map(file => {
       try {
         unlinkSync(file.path);
